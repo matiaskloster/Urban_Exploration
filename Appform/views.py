fe2f6a4a6463af3569dcce538388fae4ac05b702
matias kloster <mklosterdba@gmail.com>
2021-04-13 22:38:32 -0300

    ninth commit

--- Appform/views.py
+++ Appform/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from .models import Formmode
 from .forms import Formulario
 from .forms import forms
+from django.contrib import messages
 
 # Create your views here.
 
@@ -15,7 +16,9 @@ def vistaform(request):
             message = form.cleaned_data["message"]
             newform = Formmode(name=name, email=email, message=message)
             newform.save()
+            messages.success(request, "Thank you!!")
             return redirect("/form")
+
     else:
         form = Formulario()
     return render(request, "Appform/contact.html", {"form": form})
